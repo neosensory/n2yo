523b9254327654470cc29f7d3280b34abb4969a5
Scott Novich <novich@neosensory.com>
2020-05-12 15:54:51 -0500

    Full first pass implementation

--- app/src/main/java/com/neosensory/n2yoexample/MainActivity.java
+++ app/src/main/java/com/neosensory/n2yoexample/MainActivity.java
@@ -3,12 +3,59 @@ package com.neosensory.n2yoexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
+import com.neosensory.n2yo.N2YO;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    // obtain API key from https://n2yo.com -- be advised of rate limits on calls
+    static final String APIKEY = " ";
+
+    N2YO n2yo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        n2yo = new N2YO(APIKEY, this);
+        registerReceiver(responseReceiver, new IntentFilter("gotResponse"));
+
+        n2yo.getTle(25544);
+        n2yo.getSatPositions(25544, 41.702f,-76.014f,0,2);
+        n2yo.getVisualPasses(25544, 41.702f,-76.014f,0,2,5);
+        n2yo.getRadioPasses(25544, 41.702f,-76.014f,0,2,0);
+        n2yo.getWhatsUp(41.70f,-76.014f,0,2,0);
+
+        Log.i("N2YO",String.valueOf(n2yo.getID("SiriusXM")));
+        Log.i("N2YO",n2yo.getCategory(53));
     }
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(responseReceiver);
+    }
+
+    private final BroadcastReceiver responseReceiver =
+            new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    JSONObject receivedJSON = null;
+                    Bundle bundle = intent.getExtras();
+                    N2YO.CallId requestType = (N2YO.CallId) bundle.getSerializable("requestType");
+                    try {
+                        receivedJSON = new JSONObject((String) bundle.getString("responseObject"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String receivedJSONString = receivedJSON.toString();
+                    Log.i("N2YO","API Call Type: " + requestType.name() + " Response: " + receivedJSONString);
+                }
+            };
+
 }
--- n2yo/src/main/java/com/neosensory/n2yo/N2YO.java
+++ n2yo/src/main/java/com/neosensory/n2yo/N2YO.java
@@ -2,8 +2,9 @@ package com.neosensory.n2yo;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
+import android.os.Bundle;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -11,70 +12,215 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class N2YO {
-    static final String[] CATEGORIES = {"Brightest", "ISS", "Weather",
-            "NOAA", "GOES", "Earth Resources", "Search and Rescue", "Disaster Monitoring",
-            "Tracking Data Relay", "Geostationary", "Intelsat", "Gorizont", "Raduga", "Molniya",
-            "Iridium", "Orbcomm", "Globalstar", "Amateur Radio", "Experimental", "GPS Ops",
-            "Glosnass Ops", "Galileo", "Augmentation System", "Navy Nav", "Russian LEO Nav",
-            "Space Earth Sci", "Geodetic", "Engineering", "Education", "Military", "Radar Calibration",
-            "CubeSats", "SiriusXM", "TV", "Beidou Nav Sys", "Yaogan", "Westford Needles", "Parus",
-            "Strela", "Gonets", "Tsiklon", "Tsikada", "O3B Networks", "Tselina", "Celestis", "IRNSS",
-            "QZSS", "Flock", "Lemur", "GPS Constellation", "Glosnass Constellation", "Starlink",
-            "OneWeb"};
 
-    private String apiKey="";
-    private RequestQueue mRequestQueue;
-    protected Context context;
+    public static List<String> CATEGORIESLIST;
 
-    // create an Intent to broadcast Neosensory CLI Output.
-    private void broadcastWhatsUp(JSONObject whatsUp) {
-        Intent intent = new Intent("gotWhatsUp");
-        intent.putExtra("whatsUp", whatsUp);
-        context.sendBroadcast(intent);
+    public static final String[] CATEGORIES = {"ANY",
+    "Brightest",
+    "ISS",
+    "Weather",
+    "NOAA",
+    "GOES",
+    "Earth Resources",
+    "Search and Rescue",
+    "Disaster Monitoring",
+    "Tracking Data Relay",
+    "Geostationary",
+    "Intelsat",
+    "Gorizont",
+    "Raduga",
+    "Molniya",
+    "Iridium",
+    "Orbcomm",
+    "Globalstar",
+    "Amateur Radio",
+    "Experimental",
+    "GPS Ops",
+    "Glosnass Ops",
+    "Galileo",
+    "Augmentation System",
+    "Navy Nav",
+    "Russian LEO Nav",
+    "Space Earth Sci",
+    "Geodetic",
+    "Engineering",
+    "Education",
+    "Military",
+    "Radar Calibration",
+    "CubeSats",
+    "SiriusXM",
+    "TV",
+    "Beidou Nav Sys",
+    "Yaogan",
+    "Westford Needles",
+    "Parus",
+    "Strela",
+    "Gonets",
+    "Tsiklon",
+    "Tsikada",
+    "O3B Networks",
+    "Tselina",
+    "Celestis",
+    "IRNSS",
+    "QZSS",
+    "Flock",
+    "Lemur",
+    "GPS Constellation",
+    "Glosnass Constellation",
+    "Starlink",
+    "OneWeb"
+  };
+
+  public enum CallId {
+      TLE,
+      POSITIONS,
+      VISUALPASSES,
+      RADIOPASSES,
+      WHATSUP
+  }
+
+  private String apiKey = "";
+  private RequestQueue mRequestQueue;
+  protected Context context;
+
+  public String getCategory(int id){
+      if((id>=0)&&(id<=53)){
+          return CATEGORIESLIST.get(id);
+    } else {
+      return "Error, invalid ID";
+          }
+
+  }
+
+  public int getID(String type){
+      return CATEGORIESLIST.indexOf(type);
+  }
+
+  public void getSetApiKey(String key){
+      apiKey = key;
+  }
+
+  private void broadcastResponse(JSONObject response, CallId callType){
+      Intent intent = new Intent("gotResponse");
+      Bundle bundle = new Bundle();
+      bundle.putString("responseObject", response.toString());
+      bundle.putSerializable("requestType", callType);
+      intent.putExtras(bundle);
+      context.sendBroadcast(intent);
+  }
+
+    public void getWhatsUp(float obsLat, float obsLng, float obsAlt, int searchRadius, int categoryId){
+        String requestString =
+                "https://www.n2yo.com/rest/v1/satellite/above/"
+                        + String.valueOf(obsLat)
+                        + "/"
+                        + String.valueOf(obsLng)
+                        + "/"
+                        + String.valueOf(obsAlt)
+                        + "/"
+                        + String.valueOf(searchRadius)
+                        + "/"
+                        + String.valueOf(categoryId)
+                        + "/&apiKey="
+                        + apiKey;
+        request(requestString,CallId.WHATSUP);
     }
 
-
-
-    public N2YO(String tApikey, Context context){
-        apiKey = tApikey;
-        this.context = context.getApplicationContext();
-
-        mRequestQueue = Volley.newRequestQueue(context);
-
-        String testN2YO = "https://www.n2yo.com/rest/v1/satellite/tle/25544&apiKey=HNJTUZ-NG73US-PMNGZ7-4DOC";
-        JsonObjectRequest TestRequest =
-                new JsonObjectRequest(
-                        Request.Method.GET,
-                        ISS_FeedUrl,
-                        null,
-                        new Response.Listener<JSONObject>() {
-
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                try {
-                                    x = (float) response.getDouble("latitude");
-                                    y = (float) response.getDouble("longitude");
-                                    Log.i("N2YO", String.valueOf(x));
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        },
-                        new Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // TODO: Handle error
-
-                            }
-                        });
-        mRequestQueue.add(TestRequest);
-
+    public void getRadioPasses(int satID, float obsLat, float obsLng, float obsAlt, int days, int minElevation){
+        String requestString =
+                "https://www.n2yo.com/rest/v1/satellite/radiopasses/"
+                        + String.valueOf(satID)
+                        + "/"
+                        + String.valueOf(obsLat)
+                        + "/"
+                        + String.valueOf(obsLng)
+                        + "/"
+                        + String.valueOf(obsAlt)
+                        + "/"
+                        + String.valueOf(days)
+                        + "/"
+                        + String.valueOf(minElevation)
+                        + "/&apiKey="
+                        + apiKey;
+        request(requestString,CallId.RADIOPASSES);
     }
 
+  public void getVisualPasses(int satID, float obsLat, float obsLng, float obsAlt, int days, int minVisibility){
+      String requestString =
+              "https://www.n2yo.com/rest/v1/satellite/visualpasses/"
+                      + String.valueOf(satID)
+                      + "/"
+                      + String.valueOf(obsLat)
+                      + "/"
+                      + String.valueOf(obsLng)
+                      + "/"
+                      + String.valueOf(obsAlt)
+                      + "/"
+                      + String.valueOf(days)
+                      + "/"
+                      + String.valueOf(minVisibility)
+                      + "/&apiKey="
+                      + apiKey;
+      request(requestString,CallId.VISUALPASSES);
+  }
 
+  public void getSatPositions(int satID, float obsLat, float obsLng, float obsAlt, int seconds) {
+    String requestString =
+        "https://www.n2yo.com/rest/v1/satellite/positions/"
+            + String.valueOf(satID)
+            + "/"
+            + String.valueOf(obsLat)
+            + "/"
+            + String.valueOf(obsLng)
+            + "/"
+            + String.valueOf(obsAlt)
+            + "/"
+            + String.valueOf(seconds)
+            + "/&apiKey="
+            + apiKey;
+      request(requestString,CallId.POSITIONS);
+  }
+
+  public void getTle(int satID) {
+    String requestString =
+        "https://www.n2yo.com/rest/v1/satellite/tle/" + String.valueOf(satID) + "&apiKey=" + apiKey;
+    request(requestString,CallId.TLE);
+  }
+
+  private void request(String requestString, final CallId callName) {
+
+    JsonObjectRequest theRequest =
+        new JsonObjectRequest(
+            Request.Method.GET,
+            requestString,
+            null,
+            new Response.Listener<JSONObject>() {
+              @Override
+              public void onResponse(JSONObject response) {
+                broadcastResponse(response, callName);
+              }
+            },
+            new Response.ErrorListener() {
+              @Override
+              public void onErrorResponse(VolleyError error) {
+              }
+            });
+    theRequest.setRetryPolicy(
+        new DefaultRetryPolicy(
+            6000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+    mRequestQueue.add(theRequest);
+  }
+
+  public N2YO(String tApikey, Context context) {
+    apiKey = tApikey;
+    this.context = context.getApplicationContext();
+    mRequestQueue = Volley.newRequestQueue(context);
+    CATEGORIESLIST = Arrays.asList(CATEGORIES);
+  }
 }
